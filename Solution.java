0635df483ce74e2658430b358dbeb18f42197a71
BhargaviAkella <47807373+BhargaviAkella@users.noreply.github.com>
2019-03-08 14:56:46 -0600

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int s =0;
+       HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+
+       for(int i : ar)
+       {
+           if(hm.containsKey(i))
+           {
+               hm.put(i,hm.get(i)+1);
+           }
+           else
+           hm.put(i,1);
+       }  
+        Set<Integer> set = hm.keySet();
+        {
+            for(int i : set)
+            {
+                if(hm.get(i)>=2)
+                {
+                s+=hm.get(i)/2;
+                }
+            }
+        }
+        
+        return s;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
